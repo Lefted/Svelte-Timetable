0514ad7b1a2fadd386f5e3858e68281bccab82dd
Moritz Gartner <gartnermoritz@yahoo.de>
2022-02-16 12:31:31 +0100

    Add Tailwind.css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { activities } from "../stores/activityStore";
+	console.log($activities);
+</script>
+
+<svelte:head>
+	<title>Aktivitäten Stundenplan</title>
+</svelte:head>
+
+{#each $activities as activity}
+	<p>{activity.title}</p>
+{/each}
