b7c1677c5b6d87c2329de7f807fe0a90b57549bf
Moritz Gartner <gartnermoritz@yahoo.de>
2022-02-18 10:59:49 +0100

    Add Alias For Models

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
 import adapter from '@sveltejs/adapter-auto';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			resolve: {
+				alias: {
+					$models: path.resolve('./src/models')
+				}
+			}
+		}
 	}
 };
 
